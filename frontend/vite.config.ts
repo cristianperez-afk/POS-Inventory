d3d6a65fcae032a3693caed0ec31ba6d42839e83
Jennyur <jennyorrjen@gmail.com>
2026-06-25 15:23:48 +0800

    update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -53,8 +53,6 @@ export default defineConfig({
       },
     },
   },
-
-    // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-    assetsInclude: ['**/*.svg', '**/*.csv'],
-  }
+  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  assetsInclude: ['**/*.svg', '**/*.csv'],
 })
